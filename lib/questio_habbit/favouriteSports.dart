60be65d3a64587d84c2be88fdf9b24eb21fff811
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-28 15:00:01 +0530

    add text

--- lib/questio_habbit/favouriteSports.dart
+++ lib/questio_habbit/favouriteSports.dart
@@ -3,7 +3,20 @@ import 'package:flutter/material.dart';
 class Favouritesports extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Do you have a favourite sport?\nIf so, what is it?',
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
